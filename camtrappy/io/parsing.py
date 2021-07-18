b7e0d7b8d6ff12a85b3013febfc7f205a6e2b021
Florian Frosch <sturmpuls@posteo.de>
2021-07-18 16:38:16 +0200

    feat: parse videos & datetime from filesystem

--- camtrappy/io/parsing.py
+++ camtrappy/io/parsing.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from dataclasses import dataclass, field
+from datetime import datetime
 from pathlib import Path
 from typing import Dict, List
 
@@ -10,17 +11,18 @@ class Project:
     projectfolder: str
     datafolder: str
     videoformats: List[str] = field(default_factory=list)
-    _data: Dict[str, List[str]] = field(default_factory=defaultdict)
+    _data: Dict[str, Dict[str, List[str]]] = field(default_factory=defaultdict)
 
     def __post_init__(self):
         if len(self.videoformats) == 0:
             self.videoformats.append('mkv')
-        self._data.default_factory = list
+        self._data.default_factory = lambda: defaultdict(list)
         self.get_locations()
         for format in self.videoformats:
             self.get_videos(format)
+        self.get_datetime()
 
-    def get_locations(self) -> List[str]:
+    def get_locations(self):
         for location in Path(self.datafolder).iterdir():
             if location.is_dir():
                 self._data[str(location.relative_to(self.datafolder))]
@@ -29,13 +31,63 @@ class Project:
         locations = [Path(self.datafolder) / p for p in self._data.keys()]
         for location in locations:
             for video in Path(location).rglob(f'*.{format}'):
-                self._data[location.name].append(str(video.relative_to(location)))
+                self._data[location.name]['videos'].append(str(video.relative_to(location)))
 
-    # TODO: parse everything with full path and delegate path handling to
-    # this "data" function
-    @property
-    def data(self, full_path=False):
-        if not full_path:
-            return dict(self._data)
+    def get_datetime(self, part=-1, slice=(0,15), sep='_', clock_24h=True):
+        for location, lists in self._data.items():
+            for video in lists['videos']:
+                date, time = Path(video).parts[part][slice[0]:slice[1]].split(sep)
 
-    # TODO: get all possible information from the video path and store in lists
+                # year as "yyyy"
+                if len(date) == 8:
+                    Y = '%Y'
+                # year as "yy"
+                else:
+                    Y = '%y'
+                date = datetime.strptime(date, f'{Y}%m%d').date()
+                self._data[location]['dates'].append(date)
+
+                # hours as "00-24"
+                if clock_24h:
+                    H = '%H'
+                # hours as "00-12"
+                else:
+                    H = '%I'
+                time = datetime.strptime(time, f'{H}%M%S').time()
+                self._data[location]['times'].append(time)
+
+    def data(self, full_path=False, sort_by=None, filter_by=None):
+        """
+        sort_by : {None, location, date, time}
+        filter_by: {None, location, date, time}
+        """
+        # TODO: implement sort_by
+        # TODO: implement filter_by
+        out_data = {}
+        for location in self._data.keys():
+            location_path = location
+            video_paths = self._data[location]['videos']
+            if full_path:
+                location_path = str(Path(self.datafolder) / Path(location))
+                video_paths = [str(Path(location_path) / Path(v)) for v in video_paths]
+            out_data[location_path] = video_paths
+        return out_data
+
+    def data_flat(self):
+        out_data = []
+        for location in self._data.keys():
+            videos = self._data[location]['videos']
+            dates = self._data[location]['dates']
+            times = self._data[location]['times']
+            out_data.extend(zip([location] * len(videos), videos, dates, times))
+        return out_data
+
+
+# data-dict
+"""
+{location: {
+    videos: [],
+    dates: [],
+    times: []
+}
+"""
